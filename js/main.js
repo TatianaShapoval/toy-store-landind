aba6844e8ac0ae0f508aa077516e15f416090657
TatianaShapoval <questopunto@gmail.com>
2022-02-18 23:54:11 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+
+    // $('.products__slider').slick({
+    //     arrows: false,
+    //     slidesToShow: 4,
+    //     slidesToScroll: 3,
+    //     dots: true,
+    //     responsive: [
+    //         {
+    //           breakpoint: 676,
+    //           settings: {
+    //             slidesToScroll: 3,
+    //             slidesToShow: 3,
+    //           }
+              
+    //         },         
+    //         {
+    //           breakpoint: 475,
+    //           settings: {
+    //             slidesToScroll: 4,
+    //             slidesToShow: 2,
+    //           }
+              
+    //         },
+    //         {
+    //             breakpoint: 414,
+    //             settings: {
+    //               slidesToScroll: 6,
+    //               slidesToShow: 1,
+    //             }
+                
+    //           }
+    //       ]
+    // });
+
+    // $(".menu__btn").on("click", function () {
+    //     $(".menu-adaptive").toggleClass("menu-adaptive--active");
+    //   });
+  
